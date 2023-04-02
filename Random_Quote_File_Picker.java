7cb8192ed1873d3936808ae69794e812ef8065e7
Kooreis <kooreis.official@gmail.com>
2023-04-02 18:53:01 -0500

    Imports necessary libraries for file handling, random number generation, and data structures.

--- Random_Quote_File_Picker.java
+++ Random_Quote_File_Picker.java
@@ -1,28 +1,6 @@
-```java
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-
-public class RandomQuoteFilePicker {
-    public static void main(String[] args) {
-        List<String> quotes = new ArrayList<>();
-        try {
-            File file = new File("quotes.txt");
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                quotes.add(scanner.nextLine());
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            System.exit(0);
-        }
-        Random random = new Random();
-        int index = random.nextInt(quotes.size());
-        System.out.println(quotes.get(index));
-    }
-}
-```
