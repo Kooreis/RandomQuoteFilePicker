30a71bf27080ae98388864afd938830e0bcd64a4
Kooreis <kooreis.official@gmail.com>
2023-04-02 18:53:01 -0500

    Imported necessary modules: os for file handling and random for random selection.

--- Random_Quote_File_Picker.py
+++ Random_Quote_File_Picker.py
@@ -1,15 +1,2 @@
-```python
 import os
 import random
-
-def get_random_quote():
-    files = os.listdir("quotes")
-    random_file = random.choice(files)
-    with open(f"quotes/{random_file}", "r") as file:
-        lines = file.readlines()
-        random_line = random.choice(lines)
-        print(random_line.strip())
-
-if __name__ == "__main__":
-    get_random_quote()
-```
