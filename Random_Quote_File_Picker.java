a6b8d739a419b4934fc6c1292d3dc12c2d0ee829
Kooreis <kooreis.official@gmail.com>
2023-04-03 17:53:01 -0500

    Defines the main class and main method, and initializes an ArrayList to store the quotes.

--- Random_Quote_File_Picker.java
+++ Random_Quote_File_Picker.java
@@ -1,6 +1,3 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+public class RandomQuoteFilePicker {
+public static void main(String[] args) {
+List<String> quotes = new ArrayList<>();
